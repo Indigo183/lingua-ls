083274d4a3e80bd0aa0132d46b0307f8a5a67e94
Rua Fuller <ruafuller@gmail.com>
2025-11-29 21:06:58 +0000

    Comply with the Language Server Protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,81 @@
 pub mod rpc {
-    use lsp_types::{ClientCapabilities, InitializeParams};
-    use serde::{Deserialize, Serialize};
+    // use lsp_types::{ClientCapabilities, InitializeParams};
+    // use serde::{Deserialize, Serialize};
 
-    pub type LSPAny = serde_json::Value;
-    pub type LSPObject = std::collections::HashMap<String, LSPAny>;
+    // pub type LSPAny = serde_json::Value;
+    // pub type LSPObject = std::collections::HashMap<String, LSPAny>;
+
+    pub use tower_lsp::jsonrpc::Result;
+    pub use tower_lsp::lsp_types::*;
+    pub use tower_lsp::{Client, LanguageServer, LspService, Server};
+    pub use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+
+    #[derive(Debug)]
+    pub enum Language {
+        English,
+        Deutsch,
+    }
+
+    #[derive(Debug)]
+    pub struct Backend {
+        pub client: Client,
+        pub language: Language,
+    }
+
+    #[tower_lsp::async_trait]
+    impl LanguageServer for Backend {
+        async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+            Ok(InitializeResult {
+                server_info: Some(ServerInfo {
+                    name: String::from("Linga LS"),
+                    version: Some(String::from("pre-alpha")),
+                }),
+                capabilities: ServerCapabilities {
+                    position_encoding: None,
+                    text_document_sync: None,
+                    selection_range_provider: None,
+                    hover_provider: None,
+                    completion_provider: None,
+                    signature_help_provider: None,
+                    definition_provider: None,
+                    type_definition_provider: None,
+                    implementation_provider: None,
+                    references_provider: None,
+                    document_highlight_provider: None,
+                    document_symbol_provider: None,
+                    workspace_symbol_provider: None,
+                    code_action_provider: None,
+                    code_lens_provider: None,
+                    document_formatting_provider: None,
+                    document_range_formatting_provider: None,
+                    document_on_type_formatting_provider: None,
+                    rename_provider: None,
+                    document_link_provider: None,
+                    color_provider: None,
+                    folding_range_provider: None,
+                    declaration_provider: None,
+                    execute_command_provider: None,
+                    workspace: None,
+                    call_hierarchy_provider: None,
+                    semantic_tokens_provider: None,
+                    moniker_provider: None,
+                    linked_editing_range_provider: None,
+                    inline_value_provider: None,
+                    inlay_hint_provider: None,
+                    diagnostic_provider: None,
+                    experimental: None,
+                },
+            })
+        }
+
+        async fn initialized(&self, _: InitializedParams) {
+            self.client
+                .log_message(MessageType::INFO, "server initialized!")
+                .await;
+        }
+
+        async fn shutdown(&self) -> Result<()> {
+            Ok(())
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-fn main() {
-    use lingua_ls::rpc::*;
+use tower_lsp::{LspService, Server};
+use tracing::info;
 
-    println!("Hello, world!");
+use lingua_ls::rpc::*;
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    // File logger
+    let file_appender = tracing_appender::rolling::never("logs", "lingua.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt()
+        .with_env_filter("info")
+        .with_writer(non_blocking)
+        .init();
+
+    info!("Starting LSP server");
+
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        language: Language::English,
+    });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
